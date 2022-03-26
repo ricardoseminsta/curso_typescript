e114dbb4bf24961c2e1bd2ce902c4c313947ecfc
Ricardo <ricardo.emanuel66@gmail.com>
2022-03-25 23:24:03 -0300

    commit linux nomination

---
+++ 01_configuracoes/01_watch_mode/src/script.js
@@ -0,0 +1,5 @@
+var nome = 'Ricardo';
+function jorge(n1) {
+    return "o retorno bateu" + n1;
+}
+jorge(12);
---
+++ 01_configuracoes/01_watch_mode/src/script.ts
@@ -0,0 +1,7 @@
+const nome: string = 'Ricardo';
+
+function jorge(n1: number): string {
+    return "o retorno bateu"+ n1;
+}
+
+jorge(12);
